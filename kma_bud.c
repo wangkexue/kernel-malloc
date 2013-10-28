251c7ac7f03c40ef1dad9c2336f043097f2b0fd9
wangkexue <wang.mingwan@gmail.com>
2013-10-28 08:44:53 -0500

    there is mismatch issue

--- kma_bud.c
+++ kma_bud.c
@@ -53,6 +53,7 @@ typedef struct{
   unsigned char size;
   void* next;
   void* prev;
+  kma_page_t* page;
 } buf_t;
 
 typedef struct free_list_t{
@@ -71,6 +72,7 @@ typedef struct{
   freelst_t buf2048;
   freelst_t buf4096;
   freelst_t buf8192;
+  int in_use;
 } buf_list_t;
 /************Global Variables*********************************************/
 kma_page_t* gpage = NULL;
@@ -84,18 +86,19 @@ void delete_buf(buf_t*, freelst_t*);
 /* find the right list for certain size */
 freelst_t* find_list(int);
 /* combine two free buddy */
-void buf_union();
+void buf_union(void*);
+/* get the address of buddy */
+void* getBud(void*, unsigned char);
 /************External Declaration*****************************************/
 
 /**************Implementation***********************************************/
 
 void*
 kma_malloc(kma_size_t size)
-{
+{ 
+  //printf("REQUEST %d\n", size);
   if(gpage == NULL)
-    {
-      init_page();
-    }
+    init_page();
   //free_list_t* freelist = NULL;
   int malloc_size = size + sizeof(buf_t); 
   if(malloc_size > PAGESIZE)
@@ -103,25 +106,25 @@ kma_malloc(kma_size_t size)
   //freelst_t* freelist = find_list(malloc_size);
   freelst_t* freelist = NULL;
   buf_list_t* mainlist = (buf_list_t*)((void*)gpage->ptr + sizeof(buf_t));
-  if(size <= 32)
+  if(malloc_size <= 32)
     freelist = &mainlist->buf32;
-  else if(size <= 64)
+  else if(malloc_size <= 64)
     freelist = &mainlist->buf64;
-  else if(size <= 128)
+  else if(malloc_size <= 128)
     freelist = &mainlist->buf128;
-  else if(size <= 256)
+  else if(malloc_size <= 256)
     freelist = &mainlist->buf256;
-  else if(size <= 512)
+  else if(malloc_size <= 512)
     freelist = &mainlist->buf512;
-  else if(size <= 1024)
+  else if(malloc_size <= 1024)
     freelist = &mainlist->buf1024;
-  else if(size <= 2048)
+  else if(malloc_size <= 2048)
     freelist = &mainlist->buf2048;
-  else if(size <= 4096)
+  else if(malloc_size <= 4096)
     freelist = &mainlist->buf4096;
-  else if(size <= 8192)
+  else if(malloc_size <= 8192)
     freelist = &mainlist->buf8192;
-  //printf("%x\n", freelist);
+  //printf("SS %d\n", 1<<freelist->size);
   void* _buf = get_buf(freelist);
   return _buf;
 }
@@ -165,16 +168,6 @@ void init_page()
   mainlist->buf64.size = 6;
   mainlist->buf32.size = 5;
   
-  mainlist->buf8192.up = NULL;
-  mainlist->buf4096.up = &mainlist->buf8192;
-  mainlist->buf2048.up = &mainlist->buf4096;
-  mainlist->buf1024.up = &mainlist->buf2048;
-  mainlist->buf512.up = &mainlist->buf1024;
-  mainlist->buf256.up = &mainlist->buf512;
-  mainlist->buf128.up = &mainlist->buf256;
-  mainlist->buf64.up = &mainlist->buf128;
-  mainlist->buf32.up = &mainlist->buf64;
-
   mainlist->buf8192.first = NULL;
   mainlist->buf4096.first = NULL;
   mainlist->buf2048.first = NULL;
@@ -184,37 +177,51 @@ void init_page()
   mainlist->buf128.first = NULL;
   mainlist->buf64.first = NULL;
   mainlist->buf32.first = NULL;
+
+  mainlist->buf8192.up = NULL;
+  mainlist->buf4096.up = &mainlist->buf8192;
+  mainlist->buf2048.up = &mainlist->buf4096;
+  mainlist->buf1024.up = &mainlist->buf2048;
+  mainlist->buf512.up = &mainlist->buf1024;
+  mainlist->buf256.up = &mainlist->buf512;
+  mainlist->buf128.up = &mainlist->buf256;
+  mainlist->buf64.up = &mainlist->buf128;
+  mainlist->buf32.up = &mainlist->buf64;
   
   buf_t* buf = gpage->ptr;
   buf->next = NULL;
   buf->prev = NULL;
   mainlist->buf8192.first = buf;
   kma_malloc(sizeof(buf_list_t));
+  mainlist->in_use = 0;
 }
 
 void* get_buf(freelst_t* lst)
 {
-  //buf_list_t* list = (buf_list_t*)((void)gpage->ptr + sizeof(buf_t));
+  buf_list_t* list = (buf_list_t*)((void*)gpage->ptr + sizeof(buf_t));
+  list->in_use++;
   if(lst->first == NULL)
     {
       if(lst->up == NULL)
 	{
-	  printf("||\n");
+	  //printf("||\n");
 	  kma_page_t* page = get_page();
 	  buf_t* buf = page->ptr;
 	  buf->next = (void*)lst;
 	  buf->size = lst->size;
+	  buf->page = page;
 	  return ((void*)buf + sizeof(buf_t));
 	}
       else
 	{
-	  printf("----\n");
+	  //printf("----\n");
 	  buf_t* buf_l = (buf_t*)(get_buf(lst->up) - sizeof(buf_t));
+	  //printf("LS %d\n", 1<<lst->size);
 	  uintptr_t offset = 0x1<<lst->size;
 	  buf_t* buf_r = (buf_t*)((void*)buf_l + offset);
 	  if(lst->first != NULL)
 	    lst->first->prev = buf_r;
-	  buf_r = (void*)lst->first;
+	  buf_r->next = (void*)lst->first;
 	  lst->first = buf_r;
 	  buf_r->prev = NULL;
 	  buf_r->size = lst->size;
@@ -223,7 +230,7 @@ void* get_buf(freelst_t* lst)
  	  return ((void*)buf_l + sizeof(buf_t));
 	}
     }
-  printf("==\n");
+  //printf("==\n");
   buf_t* buf = lst->first;
   lst->first = buf->next;
   buf->next = (void*)lst;
@@ -237,7 +244,68 @@ void* get_buf(freelst_t* lst)
 void 
 kma_free(void* ptr, kma_size_t size)
 {
-  ;
+  buf_union(ptr);
+  buf_list_t* list = (buf_list_t*)((void*)gpage->ptr + sizeof(buf_t)); 
+  if(list->in_use == 0)
+    {
+      free_page(gpage);
+      gpage = NULL;
+    }
 }
 
+void buf_union(void* ptr)
+{
+  buf_list_t* list = (buf_list_t*)((void*)gpage->ptr + sizeof(buf_t));
+  list->in_use--;
+
+  buf_t* buf = (buf_t*)((void*)ptr - sizeof(buf_t));
+  freelst_t* lst = (freelst_t*)buf->next;
+  buf_t* bud = (buf_t*) getBud((void*)buf, lst->size);
+  if(bud->next == buf->next || lst->up == NULL || buf->size != bud->size)
+    // bud in use | in 8192 levle | not its bud
+    {
+      if(lst->up == NULL)
+	free_page(buf->page);
+      else
+	{
+	  if(lst->first != NULL)
+	    lst->first->prev = buf;
+	  buf->next = (void*)lst->first;
+	  buf->prev = NULL;
+	}
+    }
+  else
+    {
+      delete_buf(bud, lst);
+      if(buf > bud)
+	{
+	  
+	  buf_t* tmp = buf;
+	  buf = bud;
+	  bud = tmp;
+	}
+      buf->next = lst->up;
+      buf->size++;
+      buf_union((void*)buf + sizeof(buf_t));
+    }
+}
+
+void* getBud(void* buf, unsigned char size)
+{
+  uintptr_t size_t= 1<<size;
+  uintptr_t head= (uintptr_t)buf;
+  return (void*)(size_t ^ head);
+}
+
+void delete_buf(buf_t* buf, freelst_t* lst)
+{
+  buf_t* next = (buf_t*)buf->next;
+  buf_t* prev = (buf_t*)buf->prev;
+  if(lst->first == buf)
+      lst->first = next;
+  if(next != NULL)
+    next->prev = prev;
+  if(prev != NULL)
+    prev->next = next;
+}
 #endif // KMA_BUD
